53bb0f3bc930e676fa84349ec7ca23c83528d8a2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-18 14:33:35 -0400

    Refactor the connection to MongoDB using a function and events

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ import mongoose from "mongoose";
 const app = express();
 dotenv.config();
 
-try {
-    await mongoose.connect('mongodb://localhost:27017/test');
-} catch (error) {
-    handleError(error);
-}
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        throw error;
+    }
+};    
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB connected");
+});
 
 app.listen(8800, () => {
+    connect();
     console.log("Connected to Backend");
 });
