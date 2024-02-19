e182d2c487e00569e90c102ede5964fe71aed234
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-18 22:57:45 -0400

    Add additional middlewares for another routes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./api/routes/auth.js";
+import usersRoute from "./api/routes/users.js";
+import hotelsRoute from "./api/routes/hotels.js";
+import roomsRoute from "./api/routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -14,12 +18,15 @@ const connect = async () => {
     }
 };    
 
+// Middlewares
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
 mongoose.connection.on("disconnected", () => {
     console.log("MongoDB disconnected");
-});
-
-mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected");
 });
 
 app.listen(8800, () => {
