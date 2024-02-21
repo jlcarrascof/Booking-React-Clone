ae804d4d28178cd7df6c877c83c7c54b55809865
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-20 21:54:06 -0400

    GET route implementation: Added functionality to fetch all Hotels from the database

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -58,5 +58,13 @@ router.get("/:id", async (req, res) => {
 
 // GET ALL
 
+router.get("/", async (req, res) => {
+    try {
+        const hotel = await hotel.find();
+        res.status(200).json(hotel);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 export default router;
