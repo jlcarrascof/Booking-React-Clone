9dc7490c9e7e3d0fff22d03a41d8f2dfa9969c86
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-28 19:10:31 -0400

    Error handling improvement: The error handler in index.js has been enhanced to provide more details in error responses

--- index.js
+++ index.js
@@ -28,7 +28,14 @@ app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
-    return res.status(500).json("Hello, error from handler!");
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
 });
 
 app.listen(8800, () => {
