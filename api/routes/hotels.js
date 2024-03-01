1e0ca76fcf31b8aca88fb79adbf72eadca38609a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-29 21:27:56 -0400

    Refactoring rout create hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,22 +1,13 @@
 import express from "express";
 import Hotel from "../models/hotel.js";
 import { createError } from "../utils/error.js";
+import { createHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 
 // CREATE
 
-router.post("/", async (req, res) => {
-
-    const newHotel = new Hotel(req.body);
-
-    try {
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.post("/", createHotel);
 
 // UPDATE
 
