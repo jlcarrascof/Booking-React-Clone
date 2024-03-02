fd34a57dbc1f996312142f0c4a2272a5a3d7924a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-02 10:34:15 -0400

    Create User Model

--- api/models/User.js
+++ api/models/User.js
@@ -1,54 +1,25 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const HotelSchema = new mongoose.Schema({
-    name: {
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
         type: String,
         required: true,
     },
-    type: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    distance: { 
-        type: String,
-        required: true,
-    },
-    photos: {
-        type: String,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    desc: {
-        type: String,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 5,
-    },
-    rooms: {
-        type: [String],
-    },      
-    cheapestPrice: {
-        type: Number,
-        required: true,
-    },
-    featured: {
+    isAdmin: {
         type: Boolean,
         default: false,
     },
-});
+}, { timestamps: true });
 
-export default mongoose.model("Hotel", HotelSchema);
+export default mongoose.model("User", UserSchema);
