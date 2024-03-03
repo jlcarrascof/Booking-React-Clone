276b57c24d76186eba3b691e5a88bc82b3c65a99
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-03 13:18:20 -0400

    Configure Hotels individual route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Hotel from './pages/hotel/hotel';
 import List from './pages/list/List';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
     </BrowserRouter>
   );
