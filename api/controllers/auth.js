2e089591eb0641faafb1573dac577aed125f6d14
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-03 01:02:24 -0400

    Authentication Implementation: Added JWT token generation and error handling in routes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -52,7 +52,12 @@ export const login = async (req, res, next) => {
 
         const { password, isAdmin, ...otherDetails } = user._doc; 
 
-        res.status(200).json({...otherDetails});
+        res
+            .cookie('access_token', token, {
+                httpOnly: true,
+            })
+            .status(200)
+            .json({...otherDetails});
 
     } catch (err) {
         next(err);   
