f06da94501cdd39f6f37b6c6c45008fd07eda4ae
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-02 20:58:10 -0400

    Creating and testing login section in auth.js controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import { createError } from '../utils/error.js';
 
 export const register = async (req, res, next) => {
 
@@ -22,3 +23,31 @@ export const register = async (req, res, next) => {
 
 };
 
+export const login = async (req, res, next) => {
+
+    try {
+        const user = await User.findOne({
+            username: req.body.username,
+        });
+
+        if (!user) {
+            return next(createError(404, 'User not found'));
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password, 
+            user.password);
+
+        if (!isPasswordCorrect) {
+            return next(createError(400, 'Wrong password or username'));
+        }
+
+        const { password, isAdmin, ...otherDetails } = user._doc; 
+
+        res.status(200).json({...otherDetails});
+
+    } catch (err) {
+        next(err);   
+    }    
+
+};
