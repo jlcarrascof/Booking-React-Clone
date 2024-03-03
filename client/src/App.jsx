0afd2c98a2b39786fb7fbcc78110aca4ff09859a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-03 12:36:36 -0400

    Configure List Hotels route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import List from './pages/list/List';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
       </Routes>
     </BrowserRouter>
   );
