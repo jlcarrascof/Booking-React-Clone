2d6f8a5bb5cf4e9603b882c0dd259a86cd93bc59
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-03 20:38:59 -0400

    Added authentication check route: A route to check user authentication has been added

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { createUser, deleteUser, getUser, updateUser, getUsers } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("Hello, user! You are logged in!");
+});
 
 // CREATE
 
