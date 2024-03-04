ffd9ea7a730b912c2eac59d00877b87a31e1c95e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-03 20:44:36 -0400

    Added user check route: A route to check the user and allow them to delete their account has been added

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,6 +8,10 @@ router.get("/checkauthentication", verifyToken, (req, res, next) => {
     res.send("Hello, user! You are logged in!");
 });
 
+router.get("/checkuser/:id", verifyToken, (req, res, next) => {
+    res.send("Hello, user. You are logged in and you can delete your account!");
+});
+
 // CREATE
 
 router.post("/", createUser);
