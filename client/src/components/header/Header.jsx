b5545bb0dde09669e17fe6c1f25e1c960749fb53
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-05 12:29:19 -0400

    Use the first FontAwesome icons in our app

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,30 @@
 import '../header/header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faPlane, faCar, faTaxi } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <div className='header'>
         <div className="headerList">
             <div className="headerListItem">
-                
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car Rentals</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport Taxis</span>
             </div>
         </div>
     </div>
