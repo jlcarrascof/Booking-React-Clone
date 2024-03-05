974bebdc14956d316531c216e5d709a7c0532645
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-05 13:05:16 -0400

    Add h1, p, button to Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -28,6 +28,9 @@ const Header = () => {
                 <span>Airport Taxis</span>
             </div>
         </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+        <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or more with a free Booking account</p>        
+        <button className="headerBtn">Sign in / Register</button>
       </div>
     </div>
   )
