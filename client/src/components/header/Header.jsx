401fb27636f052998c7898ab1a830dda2c453fc2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-05 22:55:47 -0400

    Added CSS styles for search elements in header: CSS styles for the search elements in the header have been added

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
         <p className="headerDesc">Get rewarded for your travels - unlock instant savings of 10% or more with a free Booking account</p>        
         <button className="headerBtn">Sign in / Register</button>
         <div className="headerSearch">
-          <div className="headerSearchInput">
+          <div className="headerSearchItem">
             <FontAwesomeIcon icon={faBed} className='headerIcon' />
             <input 
               type="text" 
@@ -40,15 +40,15 @@ const Header = () => {
               className='headerSearchInput' 
             />
           </div>
-          <div className="headerSearchInput">
+          <div className="headerSearchItem">
             <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
             <span className='headerSearchText'>Date to date</span>
           </div>
-          <div className="headerSearchInput">
+          <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className='headerIcon' />
             <span className='headerSearchText'>2 adults 2 children 1 room</span>
           </div>
-          <div className="headerSearchInput">
+          <div className="headerSearchItem">
             <button className="headerBtn">Search</button>
           </div>
         </div>
