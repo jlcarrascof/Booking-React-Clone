daf4b86affe86ec9664765767576fb85487c460a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-10 13:05:35 -0400

    Working with Room models: getRooms

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -54,3 +54,13 @@ export const getRoom = async (req, res, next) => {
 
 };
 
+export const getRooms = async (req, res, next) => {
+
+    try {
+        const rooms = await Room.find();
+        res.status(200).json(rooms);
+    } catch (err) {
+        next(err);
+    }
+
+};
