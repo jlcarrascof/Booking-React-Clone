cdb839950189416d9bbb75c408c9fc64b54d2234
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-17 13:23:47 -0400

    Adds 'listContainer', 'listWrapper', 'listSearch', and 'listResult' to 'List' page

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -4,7 +4,18 @@ import Navbar from '../../components/navbar/Navbar';
 
 const List = () => {
   return (
-    <div><Navbar /><Header type="list" /></div>
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+          </div>
+          <div className="listResult">
+          </div>
+        </div>  
+      </div>
+    </div>
   )
 }
 
