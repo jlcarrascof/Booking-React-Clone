3e162eda1b512652af13fc57cc2287242a6d8fb4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-19 11:00:40 -0400

    Add property details to search list

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -2,7 +2,27 @@ import './searchItem.css'
 
 const SearchItem = () => {
   return (
-    <div>SearchItem</div>
+    <div className='searchItem'>
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1" 
+        alt="" 
+        className="siImg" />
+        <div className="siDesc">
+            <h1 className="siTitle">Tower Street Apartment</h1>
+            <span className="siDistance">500m from center</span>
+            <span className="siTaxiOp">Free airport taxi</span>
+            <span className="siSubtitle">
+                Studio apartment with air conditioning
+            </span>
+            <span className="siFeatures">
+                Entire studio . 1 bathroom . 21m² . 1 full bed
+            </span>
+            <span className="siCancelOp">Free cancellation</span>
+            <span className="siCancelOpSubtitle">
+                You can cancel later, so lock in this great price today!
+            </span>
+        </div>
+        <div className="siDetails"></div>
+    </div>
   )
 }
 
