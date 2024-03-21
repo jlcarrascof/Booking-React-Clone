8774b6e7075533c915dbe6cfa1383ab127072b7b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-03-20 21:49:58 -0400

    Add hotel details to hotel page

--- client/src/pages/hotel/hotel.jsx
+++ client/src/pages/hotel/hotel.jsx
@@ -1,6 +1,8 @@
 import './hotel.css'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 const Hotel = () => {
   return (
@@ -8,7 +10,19 @@ const Hotel = () => {
       <Navbar />
       <Header type="list" />
       <div className="hotelContainer">
-        
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St 125 New York</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location - 500 meters from center
+          </span>
+          <span className="hotelPriceHighlight">
+            Book a stay over $114 at this property and get a free airport taxi
+          </span>
+        </div>          
       </div>
     </div>
   )
